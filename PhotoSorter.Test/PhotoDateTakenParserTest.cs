9672c4ef0853888c55da112f8a3a78c40b6a1152
Gary Johnson <gary@gjtt.com>
2011-11-27 11:35:25 -0500

    DateTakenParser parses date from photo

---
+++ PhotoSorter.Test/PhotoDateTakenParserTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+using System.IO;
+using PhotoSorter.Test.Properties;
+
+namespace PhotoSorter.Test
+{
+    [TestClass]
+    public class PhotoDateTakenParserTest
+    {
+        [TestMethod]
+        public void Test_WhenProvidedAPhotoWithValidDate_ThenParsesCorrectDate()
+        {
+            DateTime expectedDate = DateTime.Parse("8/5/2011 3:04:42 PM");
+            DateTime actualDate = DateTime.MinValue;
+            using (MemoryStream stream = new MemoryStream(Resources.photo_with_valid_date_jpg))
+            {
+                actualDate = PhotoDateTakenParser.GetDateTaken(stream);
+            }
+
+            Assert.AreEqual(expectedDate, actualDate);
+        }
+    }
+}
---
+++ PhotoSorter/PhotoDateTakenParser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+
+namespace PhotoSorter
+{
+    public static class PhotoDateTakenParser
+    {
+        private const int DATE_TAKEN = 0x9003;
+
+        public static DateTime GetDateTaken(Stream photoStream)
+        {
+            DateTime dateTaken = DateTime.MinValue;
+
+            using (Image image = Image.FromStream(photoStream))
+            {
+                PropertyItem dateTakenItem = image.GetPropertyItem(DATE_TAKEN);
+                String dateTakenValue = Encoding.ASCII.GetString(dateTakenItem.Value);
+                dateTakenValue = dateTakenValue.Replace("\0", string.Empty);
+                dateTaken = DateTime.ParseExact(dateTakenValue, "yyyy:MM:dd HH:mm:ss", CultureInfo.CurrentCulture.DateTimeFormat);
+            }
+
+            return dateTaken;
+        }
+    }
+}
